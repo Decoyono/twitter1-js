be42cb7f252cbda6618332eaf4422d74e4a9653d
Matt Connelly <matthew.connelly9@gmail.com>
2017-04-25 10:34:06 -0400

    day 2 progress

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
 
+const volleyball = require('volleyball')
 
+app.use(volleyball)
+
+
+
+app.use('/:id', function (req, res, next) {
+console.log(req.method, ("/"+req.params.id));
+console.log(res.statusCode)
+next();
+});
+
+app.use('/special/', function (req, res, next) {
+console.log(req.method, ("/"+req.params.id));
+console.log(res.statusCode)
+console.log("You reached a special area")
+next();
+});
 
 
 app.get('/', function (req, res) {
@@ -11,6 +28,11 @@ app.get('/', function (req, res) {
 
 app.get('/news', function (req, res) {
     res.send("this is the news")
+
+});
+
+app.get('/special/', function (req, res) {
+    res.send("specialspecial")
 
 });
 
